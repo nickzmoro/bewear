e8e48cf06d8f39b1ef9a5d37c55dd37b67ae630d
nickzmoro <nicolasmororuiz@gmail.com>
2025-08-04 19:25:49 -0300

    chore: add eslint, prettier and shadcn setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
 import { dirname } from "path";
-import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +11,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
+  },
 ];
 
 export default eslintConfig;
