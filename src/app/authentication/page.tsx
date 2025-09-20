58d6ecb270db2b0afca54df0f0f0ddc0b1a3943b
nickzmoro <nicolasmororuiz@gmail.com>
2025-09-20 13:25:18 -0300

    feat: add background and logo SVGs; enhance authentication page layout with responsive design and improved user experience

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -1,23 +1,67 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import SignInForm from "./components/sign-in-form";
 import SignUpForm from "./components/sign-up-form";
+import Header from "@/components/common/header";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const Authentication = () => {
   return (
-    <div className="flex w-full flex-col gap-6 p-5">
-      <Tabs defaultValue="sign-in">
-        <TabsList>
-          <TabsTrigger value="sign-in">Entrar</TabsTrigger>
-          <TabsTrigger value="sign-up">Criar conta</TabsTrigger>
-        </TabsList>
-        <TabsContent value="sign-in" className="w-full">
-          <SignInForm />
-        </TabsContent>
-        <TabsContent value="sign-up" className="w-full">
-          <SignUpForm />
-        </TabsContent>
-      </Tabs>
-    </div>
+    <>
+      <div className="flex min-h-screen">
+        <div className="relative hidden lg:flex lg:w-1/2">
+          <Image
+            src="/bg-auth.svg"
+            alt="bg-auth"
+            fill
+            className="rounded-r-2xl object-cover object-top"
+            priority
+          />
+          <div className="absolute top-8 left-8 z-10">
+            <Link href="/">
+              <Image
+                src="/logo-branca.svg"
+                alt="logo"
+                width={120}
+                height={40}
+                className="h-auto"
+              />
+            </Link>
+          </div>
+        </div>
+        <div className="flex w-full flex-col items-center justify-center p-8 max-sm:mt-10 max-sm:justify-start lg:w-1/2">
+          <div className="mb-10 flex flex-col items-center max-sm:mb-8">
+            <Image
+              src="/logo.svg"
+              alt="logo"
+              width={120}
+              height={40}
+              className="h-auto"
+            />
+            <p className="text-muted-foreground mt-5 max-w-2/3 text-center text-sm max-sm:max-w-[80%]">
+              Olá, seja bem-vindo! Faça login ou crie sua conta e faça parte de
+              nós!
+            </p>
+          </div>
+
+          <div className="w-full max-w-md">
+            <Tabs defaultValue="sign-in" className="w-full">
+              <TabsList className="grid w-full grid-cols-2">
+                <TabsTrigger value="sign-in">Entrar</TabsTrigger>
+                <TabsTrigger value="sign-up">Criar conta</TabsTrigger>
+              </TabsList>
+              <TabsContent value="sign-in" className="mt-3">
+                <SignInForm />
+              </TabsContent>
+              <TabsContent value="sign-up" className="mt-3">
+                <SignUpForm />
+              </TabsContent>
+            </Tabs>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
